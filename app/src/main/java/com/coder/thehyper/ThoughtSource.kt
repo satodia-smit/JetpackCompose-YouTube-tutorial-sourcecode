fc212f14803a65319615ae8c7114e5b50f02df79
smit_satodia <smit.satodia@rampgroup.com>
2021-03-06 21:42:22 +0530

    Alert dialog + Toast demo

---
+++ app/src/main/java/com/coder/thehyper/ThoughtSource.kt
@@ -0,0 +1,22 @@
+package com.coder.thehyper
+
+fun listForCard1() = mutableListOf(
+    "he/she is not considering your immense love",
+    "he/she is disrespecting you everyday",
+    "he/she is hanging out with others CLOSELY all day/night long",
+    "he/she is going against in every matter",
+    "he/she is making you uncomfortable by having unethical relationship with others",
+    "he/she is commenting you on your appearance and looks",
+    "he/she is feeling happy inside, by seeing you crying for him/her",
+    "he/she is hiding the secret of their life"
+)
+
+fun listForCard2() = mutableListOf(
+    "he/she is always doing against the words they committed",
+    "he/she is making critical relationships mistakes and then repeating it again",
+    "he/she don't have any issue if you are in any relationship other then him/her",
+    "he/she is asking money from you",
+    "he/she is thinking extra marital affairs are healthy!!!",
+    "he/she is comparing you with some other handsome boy/beautiful girl always",
+    "he/she keep doing things even if you feel insecure by that EVERYDAY",
+)
---
+++ app/src/main/java/com/coder/thehyper/Thoughts.kt
@@ -0,0 +1,3 @@
+package com.coder.thehyper
+
+class Thoughts(var aIamgeRes: Int, var aThought: String)
